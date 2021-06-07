8ac72c9523ecaed8898ac0fc446fa8a6814d8e24
YongChenSu <yongchensu0@gmail.com>
2021-06-07 20:46:28 +0800

    Add todo routes

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => {
-    res.send('hello');
-});
+const todoController = require('./controllers/todo');
 
-app.get('/hello', (req, res) => {
-    res.send('hello man');
-});
+app.set('view engine', 'ejs');
 
-app.get('/bye', (req, res) => {
-    res.send('yo man');
-});
+// 若專案夠大，可考慮將路由移出
+app.get('/todos', todoController.getAll);
+app.get('/todos/:id', todoController.get);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
