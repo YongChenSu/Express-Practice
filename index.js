598364e01054d42e39ec2b9eec22c6b908c0195d
YongChenSu <yongchensu0@gmail.com>
2021-06-11 14:09:32 +0800

    Add checkPermission middleware

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ const todoController = require('./controllers/todo');
 
 app.set('view engine', 'ejs');
 
-// 若專案夠大，可考慮將路由移出
-app.get('/todos', todoController.getAll);
+// express 預設不解析 post delete，也就是不解析 request body，也沒有 session 管理機制，故需倚靠 middleware
+app.use((req, res, next) => {
+    console.log('Time', new Date());
+    next(); // 若是 res.end() 會直接結束，故改用 next()，把控制權交給下一個 middleware
+});
+
+// 權限管理
+function checkPermission(req, res, next) {
+    if (req.query.admin === '1') {
+        next();
+    } else {
+        return res.end('Error');
+    }
+}
+
+// 若是以下註解的寫法，所有路由都會被影響到
+// app.use(checkPermission);
+
+// 可將 function 加在一個路由，僅該路由會受影響
+app.get('/todos', checkPermission, todoController.getAll);
 app.get('/todos/:id', todoController.get);
+
+// 但有內建 query 的 middleware
+app.get('/test', (req, res) => {
+    console.log(req.query);
+});
 
 app.listen(port, () => {
     db.connect();
