0ece22637f2394da73bb61ea6bfede20d47fc626
YongChenSu <yongchensu0@gmail.com>
2021-06-11 15:47:39 +0800

    Require express-sesssion . Add login, logout route.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const db = require('./db');
 const app = express();
 const port = 5001;
@@ -16,10 +17,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application.json
 app.use(bodyParser.json());
 
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
 app.post('/todos', todoController.newTodo);
 app.get('/todos', todoController.getAll);
 app.get('/todos/:id', todoController.get);
 app.get('/', todoController.addTodo);
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// req.session, res.redirect
+app.post('/login', (req, res) => {
+    if (req.body.password === 'abc') {
+        req.session.isLogin = true;
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.isLogin = false;
+    res.redirect('/');
+});
 
 app.listen(port, () => {
     db.connect();
