6db41ccacb4ce44faea7409908bd8905a14a3a3c
YongChenSu <yongchensu0@gmail.com>
2021-06-11 13:36:59 +0800

    Update controllers and models with callback function to get db data

--- controllers/todo.js
+++ controllers/todo.js
@@ -2,16 +2,20 @@ const todoModel = require('../models/todo');
 
 const todoController = {
     getAll: (req, res) => {
-        const todos = todoModel.getAll();
-        res.render('todos', { todos });
+        todoModel.getAll((error, results) => {
+            if (error) return console.log(err);
+            res.render('todos', { todos: results });
+        });
     },
 
     get: (req, res) => {
         const id = req.params.id;
-        const todo = todoModel.get(id);
-
-        res.render('todo', {
-            todo,
+        todoModel.get(id, (error, results) => {
+            if (error) return console.log(error);
+            res.render('todo', {
+                // 因為單一個 todo 也是個陣列，所以要 index 0
+                todo: results[0],
+            });
         });
     },
 };
