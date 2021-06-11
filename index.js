3d4524f75dc6ba45e35a570479e10dd4afb83baa
YongChenSu <yongchensu0@gmail.com>
2021-06-11 16:23:49 +0800

    Require connect -flash and  Use locals

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const flash = require('connect-flash');
 const db = require('./db');
 const app = express();
 const port = 5001;
@@ -9,14 +10,8 @@ const todoController = require('./controllers/todo');
 
 app.set('view engine', 'ejs');
 
-// 要有 body-parser 才能處理 post 打過來的資料
-
-// parse application/x-wwww-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application.json
 app.use(bodyParser.json());
-
 app.use(
     session({
         secret: 'keyboard cat',
@@ -24,6 +19,15 @@ app.use(
         saveUninitialized: true,
     })
 );
+app.use(flash());
+// 自己新增 middleware
+app.use((req, res, next) => {
+    // 設定給 view 用的全域變數是 res.locals
+    // 拿 session 或是 flash 則都是 req
+    res.locals.isLogin = req.session.isLogin || false;
+    res.locals.errorMessage = req.flash('errorMessage');
+    next();
+});
 
 app.post('/todos', todoController.newTodo);
 app.get('/todos', todoController.getAll);
@@ -31,15 +35,24 @@ app.get('/todos/:id', todoController.get);
 app.get('/', todoController.addTodo);
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render(
+        'login'
+        // 若使用 locals 以下可省略
+        // , {
+        //     // 將 error message 的參數傳入
+        //     errorMessage: req.flash('errorMessage'),
+        // }
+    );
 });
 
-// req.session, res.redirect
 app.post('/login', (req, res) => {
     if (req.body.password === 'abc') {
+        // req.session 可讀可寫
         req.session.isLogin = true;
         res.redirect('/');
     } else {
+        // req.flash 對 key 寫 value
+        req.flash('errorMessage', 'Please Input Correct Password');
         res.redirect('/login');
     }
 });
